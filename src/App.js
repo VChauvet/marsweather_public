7b01daa8342b8d1eb205027fab6f5f752eec43c9
Valentin Chauvet <valentin.chauvet+git@gmail.com>
2020-09-01 16:46:07 +0200

    initial react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import WeatherCard from './Components/WeatherCard'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherCard date="01-09-2020"></WeatherCard>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/WeatherCard.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function WeatherCard (props) {
+    return(
+        <div>test weather card</div>
+    )
+}
+export default WeatherCard
